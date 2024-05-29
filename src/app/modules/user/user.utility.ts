50fe60df0447887d1dfb6fb778a671baac9d5b0e
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-05-29 20:04:14 +0600

    fix academic semester bugs

--- src/app/modules/user/user.utility.ts
+++ src/app/modules/user/user.utility.ts
@@ -12,11 +12,26 @@ const findLastStudentId = async () => {
       createdAt: -1,
     })
     .lean()
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+  //2025 01 0001
+  return lastStudent?.id ? lastStudent.id : undefined
 }
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString()
+  let currentId = (0).toString() // 0000 by default
+
+  const lastStudentId = await findLastStudentId() //2025 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(6) //01
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4) // 2025
+  const currentStudentSemesterCode = payload.code //01
+  const currentStudentSemesterYear = payload.year //2025
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentStudentSemesterCode &&
+    lastStudentSemesterYear === currentStudentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6)
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0')
   incrementId = `${payload.year}${payload.code}${incrementId}`
   return incrementId
