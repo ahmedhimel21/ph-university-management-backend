9003b22f2249603a04498f5cc18babbd30e33698
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-05-30 22:57:07 +0600

    transaction and rollback implemented

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface'
 import AcademicSemester from '../academicSemester/academicSemester.model'
@@ -6,32 +7,49 @@ import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import User from './user.model'
 import { generateStudentId } from './user.utility'
+import AppError from '../../errors/AppError'
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
-  // creating user
-  const createUser: Partial<TUser> = {}
-  // add password
-  createUser.password = password || config.default_pass
-  // add role
-  createUser.role = 'student'
+  const session = await mongoose.startSession()
+  try {
+    //start transaction
+    session.startTransaction()
+    // creating user
+    const createUser: Partial<TUser> = {}
+    // add password
+    createUser.password = password || config.default_pass
+    // add role
+    createUser.role = 'student'
 
-  // find academic semester
-  const admissionSemester = await AcademicSemester.findById(
-    studentData.admissionSemester,
-  )
-  // add generated id
-  createUser.id = await generateStudentId(
-    admissionSemester as TAcademicSemester,
-  )
-  const newUser = await User.create(createUser)
+    // find academic semester
+    const admissionSemester = await AcademicSemester.findById(
+      studentData.admissionSemester,
+    )
+    // add generated id
+    createUser.id = await generateStudentId(
+      admissionSemester as TAcademicSemester,
+    )
+    // creating user(transaction-1)
+    const newUser = await User.create([createUser], { session })
 
-  // create student
-  if (Object.keys(newUser).length) {
-    studentData.id = newUser.id
-    studentData.user = newUser._id
-    // creating student
-    const newStudent = await Student.create(studentData)
+    // create student
+    if (!newUser.length) {
+      throw new AppError(500, 'Failed to create user!')
+    }
+    studentData.id = newUser[0].id
+    studentData.user = newUser[0]._id
+
+    // creating student(transaction-2)
+    const newStudent = await Student.create([studentData], { session })
+    if (!newStudent.length) {
+      throw new Error('Failed to create student!')
+    }
+    await session.commitTransaction()
+    await session.endSession()
     return newStudent
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
   }
 }
 export const UserServices = {
